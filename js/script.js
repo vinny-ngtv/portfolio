be8392789c2659922154dea67efaef2cdad6e6d1
vinny-ngtv <vinny.ngtv@gmail.com>
2020-12-16 17:54:23 -0300

    modal no scroll background

--- js/script.js
+++ js/script.js
@@ -8,8 +8,11 @@ function modalOpen(element) {
     modalImage.src = element.src;
     // modalDescription.innerHTML = element.alt;
     modalDescription.innerHTML = "<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p><p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>";
+
+    document.body.classList.add("modal-open");
 }
 
 function modalClose() {
     modal.style.display = "none";
+    document.body.classList.remove("modal-open");
 }
