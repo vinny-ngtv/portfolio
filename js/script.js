da49eeb47a72160b2f70640e6d7b21fbd425cacc
vinny-ngtv <vinny.ngtv@gmail.com>
2020-12-21 12:11:13 -0300

    changing content

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ function modalOpen(element) {
 
     let modalImage = document.getElementById("modal_img");
     let modalDescription = document.getElementById("modal_txt");
-    modalImage.src = element.src;
-    // modalDescription.innerHTML = element.alt;
-    modalDescription.innerHTML = "<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p><p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>";
 
     document.body.classList.add("modal-open");
 }
